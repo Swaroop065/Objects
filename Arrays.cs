d01bb96f842146ee0501a3b6df7ed1fe22e536da
Swaroop065 <swaroopgowda915@gmail.com>
2022-08-04 10:03:21 +0530

    Arrays program

---
+++ Arrays.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object
+{
+    public  class Arrays
+    {
+      public static void ArrayMethods()
+        {
+            string[] books = { "TextBook", "NoteBook", "DrawingBook" };
+            int[] bookId = {1,2,3};
+            Console.WriteLine(books[0]);
+            Console.WriteLine(bookId[1]);
+        }
+
+        public static void Main()
+        {
+            ArrayMethods();
+        }
+    }
+}
